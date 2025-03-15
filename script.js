ca11e0959f7cda6e74a895f0d2420576038da1dd
Seifallahbhj <seifallah.bhj@gmail.com>
2025-03-15 01:19:57 +0100

    modification-style

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-
 const latitude = 36.8065;
 const longitude = 10.1815;
 const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+
 function recupererMeteo() {
     fetch(url)
         .then(response => response.json())
-        .then(data => {     
+        .then(data => {
             const meteo = data.current_weather;
+
             document.getElementById("nom-ville").textContent = "Tunis"; 
             document.getElementById("valeur-temperature").textContent = meteo.temperature + "°C";
             document.getElementById("valeur-description").textContent = "Vent : " + meteo.windspeed + " km/h";
